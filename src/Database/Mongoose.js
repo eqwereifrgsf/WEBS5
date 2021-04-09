898fbd9684d36670c73a1d98d7ec0b09ace13e85
Youri <38426449+youridevlam@users.noreply.github.com>
2021-04-09 21:00:16 +0200

    mongodb online

--- src/Database/Mongoose.js
+++ src/Database/Mongoose.js
@@ -1,6 +1,6 @@
 const Mongoose = require('mongoose');
 
-Mongoose.connect('mongodb://localhost:27017/PoC-Mongoose', {
+Mongoose.connect('mongodb+srv://mongoun:mongopw@cluster0.yxmsq.mongodb.net/webs5?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
