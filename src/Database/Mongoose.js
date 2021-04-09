cded6b29d2a2fb053bfc0e6dc4cbe22353244102
Youri <38426449+youridevlam@users.noreply.github.com>
2021-04-09 21:55:19 +0200

    Update Mongoose.js

--- src/Database/Mongoose.js
+++ src/Database/Mongoose.js
@@ -1,6 +1,6 @@
 const Mongoose = require('mongoose');
 
-Mongoose.connect('mongodb+srv://mongoun:mongopw@cluster0.yxmsq.mongodb.net/webs5?retryWrites=true&w=majority', {
+Mongoose.connect('mongodb+srv://mongoun:mongopw@cluster0.yxmsq.mongodb.net/webs5?retryWrites=true&w=majority&ssl=true', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
