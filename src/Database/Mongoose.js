44ca364e920d4697486ee6bbca2d9849c13e49c8
Youri <38426449+youridevlam@users.noreply.github.com>
2021-04-09 21:45:54 +0200

    timeout config

--- src/Database/Mongoose.js
+++ src/Database/Mongoose.js
@@ -5,6 +5,8 @@ Mongoose.connect('mongodb+srv://mongoun:mongopw@cluster0.yxmsq.mongodb.net/webs5
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
 });
 
 module.exports = { Mongoose };
